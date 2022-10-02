478713b01ae6c56eced8845443a641c066bbf99f
dskillz07 <114158829+dskillz07@users.noreply.github.com>
2022-10-02 09:30:04 +0200

    check out my changes_sql

--- SQL Deep Dive/Subqueries/questions.sql
+++ SQL Deep Dive/Subqueries/questions.sql
@@ -6,9 +6,56 @@
 * ordered by orderid
 */
 
+-- Using JOINS
+
+select customerid,
+    firstname,
+    lastname,
+    state
+from orders
+Join customers c
+    using (customerid)
+WHERE state IN ('NY', 'OH', 'OR')
+ORDER BY orderid
+
+-- Using Subquery 
+select c.customerid,
+    c.firstname,
+    c.lastname,
+    c.state
+from orders o,
+      ( select *
+    from customers
+       ) AS c
+WHERE state IN ('NY', 'OH', 'OR')
+ORDER BY orderid
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Filter employees who have emp_no 110183 as a manager
 */
+
+--Using JOINS
+
+SELECT e.emp_no, dm.emp_no, first_name, last_name
+from employees e
+Join  dept_emp de
+    using (emp_no)
+join dept_manager dm 
+    using (dept_no)
+where dm.emp_no = 110183
+
+--Using Subqueries
+SELECT e.emp_no, first_name, last_name
+from employees e
+    where emp_no in 
+        (SELECT emp_no
+         from dept_emp de
+         where dept_no = (
+               select dept_no
+               from dept_manager
+               where emp_no =110183
+            )
+)
+
